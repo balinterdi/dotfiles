7ef01497f8de3c3cd733fbe1ae2a6edc0f2acf24
Balint Erdi <balint.erdi@gmail.com>
2016-02-26 16:03:13 +0100

    Various stuffz

--- zsh/plugins/bundler.zsh
+++ zsh/plugins/bundler.zsh
@@ -13,7 +13,7 @@ compinit -i
 # The following is based on https://github.com/gma/bundler-exec
 export BUNDLER_EDITOR=vim
 
-bundled_commands=(annotate cap capify cucumber guard middleman nanoc rackup rainbows rake rails ruby rspec shotgun spec spork thin thor unicorn unicorn_rails)
+bundled_commands=(annotate cap capify cucumber guard jekyll middleman nanoc rackup rainbows rake rails ruby rspec shotgun spec spork thin thor unicorn unicorn_rails)
 
 ## Functions
 
